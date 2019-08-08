f6b210e8c3476ec213ec2be282a695daa81cbce1
Christian Kirschberg <christiankirschberg@Christians-MacBook-Pro.local>
2019-08-08 13:29:36 +0200

    .

--- TestStaffApp/UnitTest1.cs
+++ TestStaffApp/UnitTest1.cs
@@ -1,5 +1,11 @@
 using System;
+using System.Threading.Tasks;
+using Moq;
 using Xunit;
+
+using StaffApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.Abstractions;
 
 namespace TestStaffApp
 {
@@ -8,7 +14,40 @@ namespace TestStaffApp
         [Fact]
         public void Test1()
         {
+            Assert.Equal("5", "4");
+            
+        }
 
+        [Fact]
+        public async Task Index_ReturnsAViewResult_WithAListOfBrainstormSessions()
+        {
+            //var controller = new HomeController();
+
+            // Act
+            //var result = controller.Index();
+
+            // Assert
+            //Assert.NotNull(result);
+
+
+            /*
+            // Arrange
+            var mockRepo = new Mock<StaffAppContext>();
+            mockRepo.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(GetTestSessions());
+            var controller = new HomeController(mockRepo.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+            */
         }
     }
+
+
 }
